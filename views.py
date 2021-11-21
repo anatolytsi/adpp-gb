afd471e54bb9ebcf9301060eb98b19f6ece2750b
anatolytsi <snipl33t@gmail.com>
2021-11-21 13:59:01 +0100

    added request parameter

--- views.py
+++ views.py
@@ -4,12 +4,12 @@ from wunderbar.templating import render
 class Index:
     """Index view"""
 
-    def __call__(self):
+    def __call__(self, request):
         return '200 OK', render('index.html')
 
 
 class Contact:
     """Contact view"""
 
-    def __call__(self):
+    def __call__(self, request):
         return '200 OK', render('contact.html')
