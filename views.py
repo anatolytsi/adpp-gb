759b812b6f7b102c3cf0530861e8fc1167aeb497
anatolytsi <snipl33t@gmail.com>
2021-11-21 20:43:18 +0100

    separated courses and category courses

--- views.py
+++ views.py
@@ -52,9 +52,16 @@ class Courses:
     """Courses view"""
 
     def __call__(self, request):
+        return '200 OK', render('courses.html', objects_list=site.courses)
+
+
+class CategoryCourses:
+    """Category courses view"""
+
+    def __call__(self, request):
         try:
             category = site.find_category_by_id(int(request['request_params']['id']))
-            return '200 OK', render('courses.html', objects_list=category.courses, name=category.name,
+            return '200 OK', render('category-courses.html', objects_list=category.courses, name=category.name,
                                     id=category.id)
         except KeyError:
             return '200 OK', 'No courses have been added yet'
