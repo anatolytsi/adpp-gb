15ea13c578ff855d37c3a1e5c4216f30283914c6
anatolytsi <snipl33t@gmail.com>
2021-11-21 20:59:16 +0100

    added logging to views

--- views.py
+++ views.py
@@ -1,13 +1,15 @@
-from patterns.creational_patterns import Engine
+from patterns.creational_patterns import Engine, Logger
 from wunderbar.templating import render
 
 site = Engine()
+logger = Logger('main')
 
 
 class Index:
     """Index view"""
 
     def __call__(self, request):
+        logger.log('Index render was called')
         return '200 OK', render('index.html')
 
 
@@ -15,6 +17,7 @@ class Contact:
     """Contact view"""
 
     def __call__(self, request):
+        logger.log('Contact render was called')
         return '200 OK', render('contact.html')
 
 
@@ -22,6 +25,7 @@ class Categories:
     """Categories view"""
 
     def __call__(self, request):
+        logger.log(f'Categories render was called with categories: {", ".join([cat.name for cat in site.categories])}')
         return '200 OK', render('categories.html', objects_list=site.categories)
 
 
@@ -42,9 +46,11 @@ class CreateCategory:
             new_category = site.create_category(name, category)
             site.categories.add(new_category)
 
+            logger.log(f'Category "{name}" was created')
             return '200 OK', render('categories.html', objects_list=site.categories)
         else:
             categories = site.categories
+            logger.log(f'Category creation was rendered with: {", ".join([cat.name for cat in categories])}')
             return '200 OK', render('create-category.html', categories=categories)
 
 
@@ -52,6 +58,7 @@ class Courses:
     """Courses view"""
 
     def __call__(self, request):
+        logger.log('Courses render was called')
         return '200 OK', render('courses.html', objects_list=site.courses)
 
 
@@ -61,10 +68,13 @@ class CategoryCourses:
     def __call__(self, request):
         try:
             category = site.find_category_by_id(int(request['request_params']['id']))
+            logger.log(f'Category "{category.name}" courses render was called with courses: '
+                       f'{", ".join([course.name for course in category.courses])}')
             return '200 OK', render('category-courses.html', objects_list=category.courses, name=category.name,
                                     id=category.id)
         except KeyError:
-            return '200 OK', 'No courses have been added yet'
+            logger.error('No courses have been added yet')
+            return '400 Bad Request', 'No courses have been added yet'
 
 
 class CreateCourse:
@@ -84,6 +94,8 @@ class CreateCourse:
                 new_course = site.create_course('record', name, category)
                 site.courses.add(new_course)
 
+                logger.log(f'Course "{name}" was created')
+
                 return '200 OK', render('category-courses.html', objects_list=category.courses, name=category.name,
                                         id_=category.id)
             return '400 Bad Request', 'Course Category was not specified'
@@ -91,9 +103,11 @@ class CreateCourse:
             try:
                 self.category_id = int(request['request_params']['id'])
                 category = site.find_category_by_id(self.category_id)
+                logger.log(f'Courses creation for category {category} was rendered')
 
                 return '200 OK', render('create-course.html', name=category.name, id=category.id)
             except KeyError:
+                logger.error('Either no category ID was provided or the category does not exist')
                 return '400 Bad Request', 'Either no category ID was provided or the category does not exist'
 
 
@@ -113,6 +127,9 @@ class CopyCourse:
                 old_course.category.courses.append(new_course)
                 site.courses.add(new_course)
 
+                logger.log(f'Course "{name}" was copied')
+
             return '200 OK', render('courses.html', objects_list=site.courses)
         except KeyError:
+            logger.error('No courses have been added yet')
             return '400 Bad Request', 'No courses have been added yet'
