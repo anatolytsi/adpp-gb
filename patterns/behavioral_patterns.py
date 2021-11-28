c92fc4804284a2022421a6bd706e4a8d833bbdaf
anatolytsi <snipl33t@gmail.com>
2021-11-28 12:02:20 +0100

    added CBV templates

--- patterns/behavioral_patterns.py
+++ patterns/behavioral_patterns.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from wunderbar.templating import render
+
 
 class Observer(ABC):
     """Abstract observer class"""
@@ -45,3 +47,108 @@ class EmailSender(Observer):
         :param course: observed course
         """
         print(f'EMAIL: user {course.students[-1].name} joined course {course.name}')
+
+
+# поведенческий паттерн - Шаблонный метод
+class TemplateView:
+    """Base template view"""
+    template_name = 'template.html'
+
+    def get_context_data(self) -> dict:
+        """
+        Gets context data
+        :return: context data
+        """
+        return {}
+
+    def get_template(self) -> str:
+        """
+        Gets template name
+        :return: template name
+        """
+        return self.template_name
+
+    def render_template_with_context(self):
+        """
+        Renders template with context
+        :return: status code, view
+        """
+        template_name = self.get_template()
+        context = self.get_context_data()
+        return '200 OK', render(template_name, **context)
+
+    def __call__(self, request):
+        """
+        Renders template with context
+        :return: status code, view
+        """
+        return self.render_template_with_context()
+
+
+class ListView(TemplateView):
+    """List view"""
+    queryset = []
+    template_name = 'list.html'
+    context_object_name = 'objects_list'
+
+    def get_queryset(self) -> list:
+        """
+        Gets queryset
+        :return: queryset
+        """
+        print(self.queryset)
+        return self.queryset
+
+    def get_context_object_name(self) -> str:
+        """
+        Gets object context name
+        :return: object context name
+        """
+        return self.context_object_name
+
+    def get_context_data(self):
+        """
+        Gets context data with query set
+        :return: context data
+        """
+        queryset = self.get_queryset()
+        context_object_name = self.get_context_object_name()
+        context = {context_object_name: queryset}
+        return context
+
+
+class CreateView(TemplateView, ABC):
+    """Create view"""
+    template_name = 'create.html'
+
+    @staticmethod
+    def get_request_data(request: dict):
+        """
+        Gets request data from request
+        :param request: request
+        :return: request data
+        """
+        return request['data']
+
+    @abstractmethod
+    def create_obj(self, data: dict):
+        """
+        Abstract method to create object from data
+        :param data: creation data
+        """
+        pass
+
+    def __call__(self, request):
+        """
+        Returns a page on GET and creates an object on POST requests
+        :param request: request
+        :return: status code, view
+        """
+        if request['method'] == 'POST':
+            # метод пост
+            data = self.get_request_data(request)
+            self.create_obj(data)
+
+            return self.render_template_with_context()
+        else:
+            return super().__call__(request)
